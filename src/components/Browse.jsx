ea16e53bbaad8728283d2c05fa3127c16bf1168c
Jayasuriya-R <suryaabi3801@gmail.com>
2025-04-23 22:37:09 +0530

    Made api call with TMDB api for movie data

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Header from './Header'
+import { API_Options } from '../utils/constant'
+import { useDispatch } from 'react-redux'
+import { addNowPlayingMovie } from '../utils/movieSlice'
 
 const Browse = () => {
+
+  const dispatch = useDispatch()
+
+  const getNowPlayingMovies = async ()=>{
+    const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?page=1',
+      API_Options
+    );
+    const jsonData = await data.json();
+    console.log(jsonData.results)
+    dispatch(addNowPlayingMovie(jsonData.results))
+  }
+
+  useEffect(()=>{
+    getNowPlayingMovies();
+  },[])
+
   return (
     <div>
       <Header/>
--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,3 +1,11 @@
 export const netflixLogo = "https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production/consent/87b6a5c0-0104-4e96-a291-092c11350111/01938dc4-59b3-7bbc-b635-c4131030e85f/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
 
 export const netflixBg = "https://assets.nflxext.com/ffe/siteui/vlv3/fa4630b1-ca1e-4788-94a9-eccef9f7af86/web/IN-en-20250407-TRIFECTA-perspective_43f6a235-9f3d-47ef-87e0-46185ab6a7e0_small.jpg"
+
+export const API_Options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyOTAwNmYxMGIyZGE3MTI0ZTZmZDFjMGQ2OTQ1ZDQ1YSIsIm5iZiI6MTc0NTQxNDI0MS44MzUsInN1YiI6IjY4MDhlODYxNmUxYTc2OWU4MWVlZmY3ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.xE377FRUr476G33P8HjKFGHEzYyAQ4dQCPhSpA54OT4'
+    }
+  };

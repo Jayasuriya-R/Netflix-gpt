16a2a62e534c0569f1d42d3be7133fa1f2bdad7e
Jayasuriya-R <suryaabi3801@gmail.com>
2025-05-14 11:13:09 +0530

    added vercel.json for routing config

--- src/components/GptSearchPage.jsx
+++ src/components/GptSearchPage.jsx
@@ -9,7 +9,7 @@ const GptSearchPage = () => {
       <div className="fixed -z-10">
         <img className="h-screen object-cover w-screen" src={logo} alt="logo" />
       </div>
-      <div className="pt-[30%] md:p-0">
+      <div className="">
       <GptSearchBar />
       <GptMovieSuggestion />
     </div>
